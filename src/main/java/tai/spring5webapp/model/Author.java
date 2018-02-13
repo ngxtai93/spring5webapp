d99c2bcbc88b35f1c30c3d1ea0a8826b056eef62
Tai Nguyen <ng.xtai93@gmail.com>
2018-02-12 22:41:28 -0600

    Add constructor

--- src/main/java/tai/spring5webapp/model/Author.java
+++ src/main/java/tai/spring5webapp/model/Author.java
@@ -17,6 +17,11 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/tai/spring5webapp/model/Book.java
+++ src/main/java/tai/spring5webapp/model/Book.java
@@ -20,6 +20,12 @@ public class Book {
         inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
     public Long getId() {
         return id;
     }
