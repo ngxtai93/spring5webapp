46a90eebf04bf009dc74d5273bada57db089d233
Tai Nguyen <ng.xtai93@gmail.com>
2018-02-12 20:36:18 -0600

    Define JPA Entities and DB relations

--- src/main/java/tai/spring5webapp/model/Author.java
+++ src/main/java/tai/spring5webapp/model/Author.java
@@ -1,14 +1,28 @@
 package tai.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
